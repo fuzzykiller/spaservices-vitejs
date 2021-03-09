20fad79e264d16b205a357be7b11add4e5baa9ba
Daniel B <daniel.betz@daniel-betz.com>
2021-03-09 20:40:04 +0100

    Add code

---
+++ src/ViteJsDevServerMiddleware.cs
@@ -0,0 +1,106 @@
+/* Copyright 2021 Daniel Betz
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.NodeServices.Npm;
+using Microsoft.AspNetCore.NodeServices.Util;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.AspNetCore.SpaServices.Extensions.Util;
+using Microsoft.AspNetCore.SpaServices.Util;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using static System.FormattableString;
+
+namespace SpaServices.ViteJsDevServer
+{
+    public static class ViteJsDevServerMiddleware
+    {
+        private const string LogCategoryName = "ViteJsDevServer";
+        private static readonly TimeSpan RegexMatchTimeout = TimeSpan.FromSeconds(5);
+
+        public static void Attach(ISpaBuilder spaBuilder, string scriptName)
+        {
+            var pkgManagerCommand = spaBuilder.Options.PackageManagerCommand;
+            var sourcePath = spaBuilder.Options.SourcePath;
+            var devServerPort = spaBuilder.Options.DevServerPort;
+            if (string.IsNullOrEmpty(sourcePath))
+            {
+                throw new ArgumentException("Cannot be null or empty", nameof(sourcePath));
+            }
+
+            if (string.IsNullOrEmpty(scriptName))
+            {
+                throw new ArgumentException("Cannot be null or empty", nameof(scriptName));
+            }
+
+            var appBuilder = spaBuilder.ApplicationBuilder;
+            var applicationStoppingToken = appBuilder.ApplicationServices.GetRequiredService<IHostApplicationLifetime>().ApplicationStopping;
+            var logger = LoggerFinder.GetOrCreateLogger(appBuilder, LogCategoryName);
+            var diagnosticSource = appBuilder.ApplicationServices.GetRequiredService<DiagnosticSource>();
+            var portTask = StartViteJsServerAsync(sourcePath, scriptName, pkgManagerCommand, devServerPort, logger, diagnosticSource, applicationStoppingToken);
+
+            var targetUriTask = portTask.ContinueWith(task => new UriBuilder("http", "localhost", task.Result).Uri);
+
+            spaBuilder.UseProxyToSpaDevelopmentServer(() =>
+            {
+                var timeout = spaBuilder.Options.StartupTimeout;
+                return targetUriTask.WithTimeout(timeout, "The Vite.js server did not start listening for requests " +
+                                                          $"within the timeout period of {timeout.TotalSeconds} seconds. " +
+                                                          "Check the log output for error information.");
+            });
+        }
+
+        private static async Task<int> StartViteJsServerAsync(
+            string sourcePath, string scriptName, string pkgManagerCommand, int portNumber, ILogger logger, DiagnosticSource diagnosticSource, CancellationToken applicationStoppingToken)
+        {
+            if (portNumber == 0)
+            {
+                portNumber = TcpPortFinder.FindAvailablePort();
+            }
+
+            logger.LogInformation($"Starting Vite.js server on port {portNumber}...");
+
+            var scriptRunner =
+                new NodeScriptRunner(sourcePath, scriptName,
+                    Invariant($"--open false --clearScreen false --host 127.0.0.1 --port {portNumber} --strictPort"), null, pkgManagerCommand, diagnosticSource, applicationStoppingToken);
+            scriptRunner.AttachToLogger(logger);
+
+            using (var stdErrReader = new EventedStreamStringReader(scriptRunner.StdOut))
+            {
+                try
+                {
+                    await scriptRunner.StdOut.WaitForMatch(
+                        new Regex("dev server running at", RegexOptions.None, RegexMatchTimeout));
+                }
+                catch (EndOfStreamException ex)
+                {
+                    throw new InvalidOperationException(
+                        $"The NPM script '{scriptName}' exited without indicating that the " +
+                        $"Vite.js server was listening for requests. The error output was: " +
+                        $"{stdErrReader.ReadAsString()}", ex);
+                }
+            }
+
+            return portNumber;
+        }
+    }
+}
---
+++ src/ViteJsDevServerMiddlewareExtensions.cs
@@ -0,0 +1,44 @@
+/* Copyright 2021 Daniel Betz
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices;
+
+namespace SpaServices.ViteJsDevServer
+{
+    public static class ViteJsDevServerMiddlewareExtensions
+    {
+        public static void UseViteJsDevServer(
+            this ISpaBuilder spaBuilder,
+            string npmScript)
+        {
+            if (spaBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(spaBuilder));
+            }
+
+            var spaOptions = spaBuilder.Options;
+
+            if (string.IsNullOrEmpty(spaOptions.SourcePath))
+            {
+                throw new InvalidOperationException(
+                    $"To use {nameof(UseViteJsDevServer)}, you must supply a non-empty value for the {nameof(SpaOptions.SourcePath)} property of {nameof(SpaOptions)} when calling {nameof(SpaApplicationBuilderExtensions.UseSpa)}.");
+            }
+
+            ViteJsDevServerMiddleware.Attach(spaBuilder, npmScript);
+        }
+    }
+}
